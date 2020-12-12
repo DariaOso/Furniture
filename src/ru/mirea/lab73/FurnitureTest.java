7495f1fc27b5d81bda23ecc82d9d5947898ac87d
DariaOso <71928567+DariaOso@users.noreply.github.com>
2020-12-12 13:43:48 +0300

    Update FurnitureTest.java

--- src/ru/mirea/lab73/FurnitureTest.java
+++ src/ru/mirea/lab73/FurnitureTest.java
@@ -2,9 +2,10 @@ package ru.mirea.lab73;
 
 public class FurnitureTest {
     public static void main(String[] args) {
-        Sofa s = new Sofa();
-        s.makePrice(30);
+        Sofa s = new Sofa(300);
+        s.makePrice();
         s.repair();
+        s.clean();
         s.buy();
     }
 }
