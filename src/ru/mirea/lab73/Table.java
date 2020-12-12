c998b9bda1f01fab89353242de059614196396b6
DariaOso <71928567+DariaOso@users.noreply.github.com>
2020-12-12 13:46:53 +0300

    Update Table.java

--- src/ru/mirea/lab73/Table.java
+++ src/ru/mirea/lab73/Table.java
@@ -1,9 +1,23 @@
 package ru.mirea.lab73;
 
-public class Table extends FurnitureShop {
-    public Table() {
+public class Table extends Furniture implements FurnitureShop{
+    public Table(int quality) {
+        super(quality);
+    }
+    public void clean(){
+
+        System.out.println("Предмет почищен  с помощью средств для чистки дерева");
+    }
+    public void makePrice() {
+        System.out.println("Предмет будет стоить : " + quality + " дублонов");
     }
 
-
+    public void buy() {
+        money = this.money;
+        money = money - quality;
+        System.out.println("Спасибо за покупку! У вас осталость: " + money + " дублонов");
+    }
 }
+
+
 
