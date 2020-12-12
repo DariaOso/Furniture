5faa72d2b814a1eebb9b265c5308e1126ec5912a
DariaOso <71928567+DariaOso@users.noreply.github.com>
2020-12-12 13:42:34 +0300

    Update FurnitureShop.java

--- src/ru/mirea/lab73/FurnitureShop.java
+++ src/ru/mirea/lab73/FurnitureShop.java
@@ -1,16 +1,10 @@
 package ru.mirea.lab73;
 
-public class FurnitureShop extends Furniture {
+public interface FurnitureShop {
 
-    void makePrice(int quality) {
-        this.quality = quality;
-        price = quality * 100;
-        System.out.println("Предмет будет стоить : " + price + " дублонов");
-    }
 
-    void buy() {
-        money = money - price;
-        System.out.println("Спасибо за покупку! У вас осталость: " + money + " дублонов");
-    }
+    public abstract void makePrice();
+
+    public abstract void buy();
 }
 
