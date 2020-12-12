93c083f24d043ea10710656146e62a140793eee6
DariaOso <71928567+DariaOso@users.noreply.github.com>
2020-12-12 13:48:27 +0300

    Update Sofa.java

--- src/ru/mirea/lab73/Sofa.java
+++ src/ru/mirea/lab73/Sofa.java
@@ -1,8 +1,20 @@
 package ru.mirea.lab73;
 
-public class Sofa extends FurnitureShop {
-    public Sofa() {
+public class Sofa extends Furniture implements FurnitureShop {
+    public Sofa(int quality) {
+        super(quality);
+    }
+    public void clean(){
+
+ System.out.println("Предмет почищен c помощью средств для чистки ткани");
+}
+    public void makePrice() {
+        System.out.println("Предмет будет стоить : " + quality + " дублонов");
     }
 
-
+    public void buy() {
+        money = this.money;
+        money = money - quality;
+        System.out.println("Спасибо за покупку! У вас осталость: " + money + " дублонов");
+    }
 }
