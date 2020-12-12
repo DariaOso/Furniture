671421effbd5dae09df0a2ede306a94cd0c613eb
DariaOso <71928567+DariaOso@users.noreply.github.com>
2020-12-12 13:45:24 +0300

    Update Chair.java

--- src/ru/mirea/lab73/Chair.java
+++ src/ru/mirea/lab73/Chair.java
@@ -1,8 +1,21 @@
 package ru.mirea.lab73;
 
-public class Chair extends FurnitureShop {
-    public Chair() {
+public class Chair extends Furniture implements FurnitureShop {
+    public Chair(int quality) {
+        super(quality);
+    }
+    public void clean() {
+
+        System.out.println("Предмет почищен с помощью средств для чистки ткани и дерева");
     }
 
-}
+    public void makePrice() {
+        System.out.println("Предмет будет стоить : " + quality + " дублонов");
+    }
 
+    public void buy() {
+        money = this.money;
+        money = money - quality;
+        System.out.println("Спасибо за покупку! У вас осталость: " + money + " дублонов");
+    }
+}
